986d56eb2e83ce46a3ea53abd589519a5e7bb1cd
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-25 07:57:14 -0700

    Task 4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" MRU Cache """
+from collections import OrderedDict
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Inherits from BaseCaching and is a
+    caching system """
+
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+        self.mru_order = OrderedDict()
+
+    def put(self, key, item):
+        """ Assigns to dictionary self.cache_data
+        the item value for the key key """
+        if not key or not item:
+            return
+
+        self.cache_data[key] = item
+        self.mru_order[key] = item
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            item_discarded = next(iter(self.mru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+
+        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
+            self.mru_order.popitem(last=False)
+
+        self.mru_order.move_to_end(key, False)
+
+    def get(self, key):
+        """ Must return value in
+        self.cache_data linked to key """
+        if key in self.cache_data:
+            self.mru_order.move_to_end(key, False)
+            return self.cache_data[key]
+        return None
