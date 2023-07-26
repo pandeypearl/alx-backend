dad757ff7744e6abd0168a20ecba25c5aa1392b6
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-26 01:41:20 -0700

    Task 1 syntax fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 
+"""
 FIFO Caching
 """
 
@@ -8,8 +8,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
     """
-    Inherits from BaseCaching and is a
-    caching system
+    a class FIFOCache that inherits from BaseCaching and is a caching system
     """
 
     def __init__(self):
@@ -21,26 +20,25 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Assigns to dictionary self.cache_data
-        the item value for the key key
+        assign to the dictionary self.cache_data
+        the item value for the key key.
         """
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
                 return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            item_discarded = self.key_indexes.pop(0)
-            del self.cache_data[item_discarded]
-            print("DISCARD:", item_discarded)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_discarded = self.key_indexes.pop(0)
+                del self.cache_data[item_discarded]
+                print("DISCARD:", item_discarded)
 
-        self.cache_data[key] = item
-        self.key_indexes.append(key)
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """
-        Must return value in
-        self.cache_data linked to key
+        return the value in self.cache_data linked to key.
         """
         if key in self.cache_data:
             return self.cache_data[key]
