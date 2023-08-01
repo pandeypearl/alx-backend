26ee22083255c2fe3f26c1115f53e9639f43780e
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-01 02:12:54 -0700

    Task 2. Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic Flask Application """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """ Configuration class for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello_world():
+    """ Render basic html file """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Determines the best match with supported laguages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
